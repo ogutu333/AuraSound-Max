2373711b4a5bbc5e09cb3ed8e44aa8240077fd20
“ogutu333” <ogutu333@gmail.com>
2024-11-06 12:45:08 +0300

    Design Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,12 @@ navToggle.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
 	navMenu.classList.remove("show-menu");
 });
+
+/* ========== CHANGE BACKGROUND HEADER ========== */
+function scrollHeader() {
+	const header = document.querySelector("header");
+
+	if (this.scrollY >= 50) header.classList.add("scroll-header");
+	else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
